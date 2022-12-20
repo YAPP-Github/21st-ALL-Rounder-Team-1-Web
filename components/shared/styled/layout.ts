24154715351d3a28bcfa00b72108d92c85dbbe2a
youngmin wi <youngminieo1005@gmail.com>
2022-12-20 21:00:58 +0900

    :lipstick: Global Font, Color Theme, Default CSS Style, MaxWidthContainer 설정

--- components/shared/styled/layout.ts
+++ components/shared/styled/layout.ts
@@ -6,3 +6,9 @@ export const BoxFlexCenter = styled.div`
 	align-items: center;
 	justify-content: center;
 `;
+
+export const MaxContainer = styled.div`
+	position: relative;
+	max-width: 1200px;
+	margin: 0 auto;
+`;
