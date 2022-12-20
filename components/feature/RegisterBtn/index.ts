10d3e369b9e794fb4575d7b3b4c1f6f9749bffd6
ChoiSeYoung <sychoi0516@naver.com>
2022-12-21 02:40:34 +0900

    :memo: RegisterBtn index.ts 수정

---
+++ components/feature/RegisterBtn/index.ts
@@ -0,0 +1,2 @@
+export { default as LargeBtn } from './LargeBtn';
+export { default as SmallBtn } from './SmallBtn';
--- components/feature/index.ts
+++ components/feature/index.ts
@@ -1,2 +1,2 @@
-export { default as LargeBtn } from './RegisterBtn/LargeBtn/index';
-export { default as SmallBtn } from './RegisterBtn/SmallBtn/index';
+export { default as LargeBtn } from './RegisterBtn/LargeBtn';
+export { default as SmallBtn } from './RegisterBtn/SmallBtn';
