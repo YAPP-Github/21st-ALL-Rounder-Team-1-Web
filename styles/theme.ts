1da18b55509a1c80c438edcd4d35f9757fc5b2db
youngmin wi <youngminieo1005@gmail.com>
2022-12-22 04:02:27 +0900

    :bento: next13 localFont 설정 후 폰트 적용(in. theme.ts)

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,36 @@
 import { css, DefaultTheme } from 'styled-components';
+import localFont from '@next/font/local';
 
+const pretendard = localFont({
+	src: [
+		{
+			path: '../public/static/fonts/Pretendard-Regular.woff2',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: '../public/static/fonts/Pretendard-Medium.woff2',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: '../public/static/fonts/Pretendard-Bold.woff2',
+			weight: '700',
+			style: 'normal',
+		},
+	],
+});
 interface Font {
 	weight: 400 | 500 | 700;
 	size: number;
 	height: number;
 	spacing: number;
+	fontFamily: string;
 }
 
-const fontStyleGenerator = ({ weight, size, height, spacing }: Font) => {
+const fontStyleGenerator = ({ weight, size, height, spacing, fontFamily }: Font) => {
 	return css`
-		font-family: Pretendard;
+		font-family: ${fontFamily};
 		font-weight: ${weight};
 		font-size: ${size}px;
 		line-height: ${height}px;
@@ -35,17 +56,71 @@ const theme: DefaultTheme = {
 		primary_003: '#0064FF',
 	},
 	fonts: {
-		headline_001: fontStyleGenerator({ weight: 700, size: 48, height: 50, spacing: -1 }),
-		headline_002: fontStyleGenerator({ weight: 700, size: 32, height: 34, spacing: -0.6 }),
-		headline_003: fontStyleGenerator({ weight: 700, size: 24, height: 26, spacing: -0.4 }),
-		headline_004: fontStyleGenerator({ weight: 700, size: 20, height: 22, spacing: -0.4 }),
-		body_oneline_001: fontStyleGenerator({ weight: 400, size: 17, height: 19, spacing: -0.4 }),
-		body_oneline_002: fontStyleGenerator({ weight: 400, size: 16, height: 18, spacing: -0.4 }),
-		body_oneline_003: fontStyleGenerator({ weight: 400, size: 15, height: 17, spacing: -0.4 }),
-		body_multiline_001: fontStyleGenerator({ weight: 400, size: 17, height: 26, spacing: -0.4 }),
-		body_multiline_002: fontStyleGenerator({ weight: 400, size: 16, height: 25, spacing: -0.4 }),
-		body_multiline_003: fontStyleGenerator({ weight: 400, size: 15, height: 24, spacing: -0.4 }),
-		button_001: fontStyleGenerator({ weight: 500, size: 16, height: 18, spacing: -0.4 }),
+		headline_001: fontStyleGenerator({ weight: 700, size: 48, height: 50, spacing: -1, fontFamily: pretendard.style.fontFamily }),
+		headline_002: fontStyleGenerator({
+			weight: 700,
+			size: 32,
+			height: 34,
+			spacing: -0.6,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		headline_003: fontStyleGenerator({
+			weight: 700,
+			size: 24,
+			height: 26,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		headline_004: fontStyleGenerator({
+			weight: 700,
+			size: 20,
+			height: 22,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_oneline_001: fontStyleGenerator({
+			weight: 400,
+			size: 17,
+			height: 19,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_oneline_002: fontStyleGenerator({
+			weight: 400,
+			size: 16,
+			height: 18,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_oneline_003: fontStyleGenerator({
+			weight: 400,
+			size: 15,
+			height: 17,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_multiline_001: fontStyleGenerator({
+			weight: 400,
+			size: 17,
+			height: 26,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_multiline_002: fontStyleGenerator({
+			weight: 400,
+			size: 16,
+			height: 25,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		body_multiline_003: fontStyleGenerator({
+			weight: 400,
+			size: 15,
+			height: 24,
+			spacing: -0.4,
+			fontFamily: pretendard.style.fontFamily,
+		}),
+		button_001: fontStyleGenerator({ weight: 500, size: 16, height: 18, spacing: -0.4, fontFamily: pretendard.style.fontFamily }),
 	},
 } as const;
 
