c56d1056cd342c1051725249d0ba924b74d9aa6f
youngmin wi <youngminieo1005@gmail.com>
2022-12-21 17:47:31 +0900

    :bento: Global Icon Assets 수정

--- public/static/icons/index.ts
+++ public/static/icons/index.ts
@@ -1,7 +1,7 @@
 export { default as TrashIcon } from './trash.svg';
-export { default as PhotoIcon } from './photo.svg';
+export { default as AddPhotoIcon } from './add_photo.svg';
 export { default as SearchIcon } from './search.svg';
 export { default as RemoveIcon } from './remove.svg';
 export { default as AddIcon } from './add.svg';
-export { default as DeleteIcon } from './delete.svg';
+export { default as CancelIcon } from './cancel.svg';
 
