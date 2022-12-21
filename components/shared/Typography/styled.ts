738876ec178164bc2e476f3f97d50046fe188609
youngmin wi <youngminieo1005@gmail.com>
2022-12-22 04:04:03 +0900

    :lipstick: Typography 공통 컴포넌트 추가

---
+++ components/shared/Typography/styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+type ComponentProps = {
+	aggressive:
+		| 'headline_001'
+		| 'headline_002'
+		| 'headline_003'
+		| 'headline_004'
+		| 'body_oneline_001'
+		| 'body_oneline_002'
+		| 'body_oneline_003'
+		| 'body_multiline_001'
+		| 'body_multiline_002'
+		| 'body_multiline_003';
+	margin?: string;
+	padding?: string;
+	color?: string;
+	lineHeight?: string;
+	align?: 'center' | 'inherit' | 'justify' | 'left' | 'right';
+	whiteSpace?: 'normal' | 'nowrap' | 'pre' | 'pre-wrap';
+};
+
+export const Component = styled.div<ComponentProps>`
+	margin: ${({ margin }) => margin && margin};
+	padding: ${({ padding }) => padding && padding};
+	${({ aggressive, theme }) => theme.fonts[aggressive]}
+	line-height: ${({ lineHeight }) => lineHeight && lineHeight};
+	text-align: ${({ align }) => align && align};
+	white-space: ${({ whiteSpace }) => whiteSpace && whiteSpace};
+`;
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,7 @@
 // Shared Components
 export { default as Header } from 'components/shared/Header';
 export { default as Footer } from 'components/shared/Footer';
+export { default as Typography } from 'components/shared/Typography';
 
 // Only Styled Components
 export * as StyledLayout from 'components/shared/styled/layout';
