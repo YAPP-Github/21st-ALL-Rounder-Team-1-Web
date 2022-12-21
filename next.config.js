92b92a7b8ef342f8da1268866f04ea6ee4c1429f
youngmin wi <youngminieo1005@gmail.com>
2022-12-22 05:35:07 +0900

    :wrench: next.config.js - styled-components 설정을 complier 를 통해 설정, swc 적용, babelrc 제거

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	compiler: {
+		styledComponents: true,
+	},
+	swcMinify: true,
 	reactStrictMode: true,
 	webpack: (config) => {
 		config.module.rules.push({
