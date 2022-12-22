45c418b3a83affc2ed64cfe5cc9db330a4d9cec5
ChoiSeYoung <sychoi0516@naver.com>
2022-12-22 23:21:50 +0900

    :lipstick: 디자인 시스템 폰트 추가

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -19,17 +19,22 @@ declare module 'styled-components' {
 			primary_003: '#0064FF';
 		};
 		fonts: {
-			headline_001: FlattenSimpleInterpolation;
+			headline_oneline_001: FlattenSimpleInterpolation;
+			headline_multiline_001: FlattenSimpleInterpolation;
 			headline_002: FlattenSimpleInterpolation;
 			headline_003: FlattenSimpleInterpolation;
 			headline_004: FlattenSimpleInterpolation;
 			body_oneline_001: FlattenSimpleInterpolation;
 			body_oneline_002: FlattenSimpleInterpolation;
 			body_oneline_003: FlattenSimpleInterpolation;
+			body_oneline_004: FlattenSimpleInterpolation;
+			body_oneline_005: FlattenSimpleInterpolation;
 			body_multiline_001: FlattenSimpleInterpolation;
 			body_multiline_002: FlattenSimpleInterpolation;
 			body_multiline_003: FlattenSimpleInterpolation;
 			button_001: FlattenSimpleInterpolation;
+			tab_001: FlattenSimpleInterpolation;
+			tab_002: FlattenSimpleInterpolation;
 		};
 	}
 }
