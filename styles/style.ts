3cb0a18b581d69cdfb4b4bc91b4679ee6838ec40
youngmin wi <youngminieo1005@gmail.com>
2022-12-28 21:54:55 +0900

    :lipstick: Black Button 에 대한 style 객체 추가

--- styles/style.ts
+++ styles/style.ts
@@ -4,10 +4,15 @@ import theme from './theme';
 const style: Styles = {
 	btnStyle: {
 		white_btn: {
-			width: `${200}px`,
+			width: `200px`,
 			backgroundColor: `${theme.colors.white}`,
 			color: `${theme.colors.gray_007}`,
 			border: `1px solid ${theme.colors.gray_004}`,
+		},
+		black_btn: {
+			width: `200px`,
+			backgroundColor: `${theme.colors.gray_007}`,
+			color: `${theme.colors.white}`,
 		},
 		blue_btn_001: {
 			width: `${200}px`,
