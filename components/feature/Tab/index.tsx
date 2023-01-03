acf04207a5611def80e63ac531bab2cf5ea83f8b
ChoiSeYoung <sychoi0516@naver.com>
2023-01-04 02:19:47 +0900

    :lipstick: 탭 요소 비활성화 UI 구현

--- components/feature/Tab/index.tsx
+++ components/feature/Tab/index.tsx
@@ -1,4 +1,34 @@
+import { Typography } from 'components/shared';
+import { TabContainer, TabElement } from './styled';
+
 const Tab = () => {
-	return <></>;
+	return (
+		<TabContainer>
+			<TabElement>
+				<Typography variant="h2" aggressive="body_oneline_005">
+					Step1
+				</Typography>
+				<Typography variant="span" aggressive="tab_002">
+					운영자 기본정보
+				</Typography>
+			</TabElement>
+			<TabElement>
+				<Typography variant="h2" aggressive="body_oneline_005">
+					Step2
+				</Typography>
+				<Typography variant="span" aggressive="tab_002">
+					매장정보
+				</Typography>
+			</TabElement>
+			<TabElement>
+				<Typography variant="h2" aggressive="body_oneline_005">
+					Step3
+				</Typography>
+				<Typography variant="span" aggressive="tab_002">
+					상품정보
+				</Typography>
+			</TabElement>
+		</TabContainer>
+	);
 };
 export default Tab;
--- components/feature/Tab/styled.ts
+++ components/feature/Tab/styled.ts
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
-
-export const TabContainer = styled.div`
+export const TabContainer = styled.nav`
 	width: 996px;
 	height: 68px;
+	display: flex;
+	margin: 50px; //ui 확인용
+`;
+export const TabElement = styled.section`
+	margin-top: 50px; //ui 확인용
+	width: 332px;
+	height: 68px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 16px 82px;
+	background-color: ${({ theme }) => theme.colors.gray_001};
+	color: ${({ theme }) => theme.colors.gray_004};
+	border-radius: 10px 10px 0px 0px;
+	& > h1 {
+		color: ${({ theme }) => theme.colors.gray_003};
+	}
 `;
