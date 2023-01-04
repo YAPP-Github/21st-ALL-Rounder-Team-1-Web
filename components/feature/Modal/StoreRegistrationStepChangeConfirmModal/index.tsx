660e7296a0a4fc82056244b6a9148f7aba08ab9e
youngmin wi <youngminieo1005@gmail.com>
2023-01-05 02:27:05 +0900

    :lipstick: 상점 입점 Step 1,2 -> 3 로 넘어가기 확인 Modal 추가

---
+++ components/feature/Modal/StoreRegistrationStepChangeConfirmModal/index.tsx
@@ -0,0 +1,22 @@
+import { ConfirmModal } from 'components/feature/Modal';
+
+const StoreRegistrationStepChangeConfirmModal = () => {
+	return (
+		<ConfirmModal
+			modalTitle="다음 단계로 이동하시겠습니까 ?"
+			modalDescription="다음 단계로 이동 시 현재 페이지의 정보 수정이 불가능합니다."
+			modalBtn={{
+				cancel: {
+					text: '취소',
+					onCancel: () => {},
+				},
+				confirm: {
+					text: '다음단계',
+					onConfirm: () => {},
+				},
+			}}
+		/>
+	);
+};
+
+export default StoreRegistrationStepChangeConfirmModal;
