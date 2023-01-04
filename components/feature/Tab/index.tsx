4b4bd23100f8d6bd21bc74df7f72294bf5ddb5fa
ChoiSeYoung <sychoi0516@naver.com>
2023-01-05 02:32:54 +0900

    :sparkles:탭 메뉴 구현을 위한 registration>layout.tsx 추가 및 탭 element  UI 구현

--- components/feature/Tab/index.tsx
+++ components/feature/Tab/index.tsx
@@ -1,33 +1,44 @@
 import { Typography } from 'components/shared';
-import { TabContainer, TabElement } from './styled';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { theme } from 'styles';
+import { SelectedTabElement, TabContainer, TabElement } from './styled';
 
 const Tab = () => {
+	const pathname = usePathname();
+	const tabElementArr = [
+		{ id: 1, step: 'Step1', title: '운영자 기본정보' },
+		{ id: 2, step: 'Step2', title: '매장정보' },
+		{ id: 3, step: 'Step3', title: '상품정보' },
+	];
+	const [currentStep, setCurrentStep] = useState(1);
+	useEffect(() => {
+		if (pathname !== null) {
+			setCurrentStep(() => Number(pathname[pathname.length - 1]));
+		}
+	}, [pathname]);
 	return (
 		<TabContainer>
-			<TabElement isCurrentStep={true}>
-				<Typography variant="h2" aggressive="body_oneline_005">
-					Step1
-				</Typography>
-				<Typography variant="span" aggressive="tab_002">
-					운영자 기본정보
-				</Typography>
-			</TabElement>
-			<TabElement isCurrentStep={false}>
-				<Typography variant="h2" aggressive="body_oneline_005">
-					Step2
-				</Typography>
-				<Typography variant="span" aggressive="tab_002">
-					매장정보
-				</Typography>
-			</TabElement>
-			<TabElement isCurrentStep={false}>
-				<Typography variant="h2" aggressive="body_oneline_005">
-					Step3
-				</Typography>
-				<Typography variant="span" aggressive="tab_002">
-					상품정보
-				</Typography>
-			</TabElement>
+			{tabElementArr.map(({ id, step, title }) => {
+				return (
+					<TabElement key={step} isSelected={id === currentStep}>
+						<Typography
+							variant="h2"
+							color={id === currentStep ? theme.colors.primary_003 : theme.colors.gray_003}
+							aggressive="body_oneline_005"
+						>
+							{step}
+						</Typography>
+						<Typography
+							variant="span"
+							color={id === currentStep ? theme.colors.gray_007 : theme.colors.gray_004}
+							aggressive="tab_002"
+						>
+							{title}
+						</Typography>
+					</TabElement>
+				);
+			})}
 		</TabContainer>
 	);
 };
--- components/feature/Tab/styled.ts
+++ components/feature/Tab/styled.ts
@@ -5,7 +5,7 @@ export const TabContainer = styled.nav`
 	display: flex;
 	margin: 50px; //ui 확인용
 `;
-export const TabElement = styled.section<{ isCurrentStep: boolean }>`
+export const TabElement = styled.section<{ isSelected: boolean }>`
 	margin-top: 50px; //ui 확인용
 	width: 332px;
 	height: 68px;
@@ -15,21 +15,11 @@ export const TabElement = styled.section<{ isCurrentStep: boolean }>`
 	align-items: center;
 	padding: 16px 82px;
 	background-color: ${({ theme }) => theme.colors.gray_001};
-	color: ${({ theme }) => theme.colors.gray_004};
 	border-radius: 10px 10px 0px 0px;
-	& > h2 {
-		color: ${({ theme }) => theme.colors.gray_003};
-	}
-	${({ isCurrentStep, theme }) =>
-		isCurrentStep &&
+	${({ isSelected, theme }) =>
+		isSelected &&
 		css`
 			box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.06);
 			background-color: ${theme.colors.white};
-			color: ${theme.colors.gray_007};
-			${theme.fonts.tab_001};
-			& > h2 {
-				color: ${theme.colors.primary_003};
-				${theme.fonts.body_oneline_005};
-			}
 		`}
 `;
