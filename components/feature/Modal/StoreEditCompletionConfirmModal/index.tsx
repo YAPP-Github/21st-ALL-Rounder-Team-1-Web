d722503231b0effff8a78be301c71871872dc1c5
youngmin wi <youngminieo1005@gmail.com>
2023-01-05 02:41:47 +0900

    :adhesive_bandage: 상점 수정 완료 확인 Modal 부연 설명 텍스트 오타 수정

--- components/feature/Modal/StoreEditCompletionConfirmModal/index.tsx
+++ components/feature/Modal/StoreEditCompletionConfirmModal/index.tsx
@@ -4,7 +4,7 @@ const StoreEditCompletionConfirmModal = () => {
 	return (
 		<ConfirmModal
 			modalTitle="수정을 완료하시겠습니까 ?"
-			modalDescription="수정사항은 매일 새벽 00:00시 일괄 반영됩니다. (실시가 반영 X)"
+			modalDescription="수정사항은 매일 새벽 00:00시 일괄 반영됩니다. (실시간 반영 X)"
 			modalBtn={{
 				cancel: {
 					text: '취소',
