4b9c7bdb5d1e9a94fb40838f1c815ac7a6c071ef
youngmin wi <youngminieo1005@gmail.com>
2023-01-08 17:55:33 +0900

    :sparkles: favicon 적용

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
 	return (
 		<>
-			<title>Hola 👋🏻</title>
+			<title>Pump</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
-			<link rel="icon" href="/favicon.ico" />
+			<link rel="shortcut icon" href="/favicon.ico" />
 		</>
 	);
 }
