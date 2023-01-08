e2673fb05bf6f3030992c57fdd0d032aa02f7073
youngmin wi <youngminieo1005@gmail.com>
2023-01-08 17:43:26 +0900

    :lipstick: 로고 이미지 추가(in. Header)

--- public/static/images/index.ts
+++ public/static/images/index.ts
@@ -1,3 +1,3 @@
 export { default as KakaoSymbolImg } from './kakao_symbol.png';
 export { default as NaverSymbolImg } from './naver_symbol.png';
-
+export { default as PumpLogo } from './pump_logo.png';
