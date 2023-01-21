9d5e51152091e4303475fa0ed4911ac49efe7cb6
youngmin wi <youngminieo1005@gmail.com>
2023-01-21 23:55:00 +0900

    :lipstick: 회원가입 페이지에 필요한 Checkbox & Label UI 컴포넌트 구현

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -5,6 +5,8 @@ export { default as LargeBtn } from 'components/shared/Button/LargeBtn';
 export { default as Typography } from 'components/shared/Typography';
 export { default as Toast } from 'components/shared/Toast';
 export { default as ModalFrame } from 'components/shared/ModalFrame';
+export { default as Checkbox } from 'components/shared/Checkbox';
+export { default as Label } from 'components/shared/Label';
 
 // Only Styled Components
 export * as StyledLayout from 'components/shared/styled/layout';
