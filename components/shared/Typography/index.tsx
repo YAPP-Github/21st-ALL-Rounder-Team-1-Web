735b826532688198bd084b5f77d5e956c652b3b7
youngmin wi <youngminieo1005@gmail.com>
2023-01-28 21:44:00 +0900

    :bento: Typography 폰트 스타일 추가

--- components/shared/Typography/index.tsx
+++ components/shared/Typography/index.tsx
@@ -17,10 +17,12 @@ type Props = {
 		| 'body_oneline_003'
 		| 'body_oneline_004'
 		| 'body_oneline_005'
+		| 'body_multiline_000'
 		| 'body_multiline_001'
 		| 'body_multiline_002'
 		| 'body_multiline_003'
 		| 'body_multiline_005'
+		| 'button_000'
 		| 'button_001'
 		| 'tab_001'
 		| 'tab_002';
--- components/shared/Typography/styled.ts
+++ components/shared/Typography/styled.ts
@@ -15,10 +15,12 @@ type ComponentProps = {
 		| 'body_oneline_003'
 		| 'body_oneline_004'
 		| 'body_oneline_005'
+		| 'body_multiline_000'
 		| 'body_multiline_001'
 		| 'body_multiline_002'
 		| 'body_multiline_003'
 		| 'body_multiline_005'
+		| 'button_000'
 		| 'button_001'
 		| 'tab_001'
 		| 'tab_002';
