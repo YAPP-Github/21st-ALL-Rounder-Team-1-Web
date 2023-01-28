454848333ce7d458e38ad84439b0e958b456f43e
youngmin wi <youngminieo1005@gmail.com>
2023-01-28 17:41:29 +0900

    :lipstick: ModalFrame Backdrop 추가

--- components/shared/ModalFrame/index.tsx
+++ components/shared/ModalFrame/index.tsx
@@ -5,25 +5,22 @@ import * as S from './styled';
 const ModalFrame = (props: PropsWithChildren) => {
 	return (
 		<ModalPortal>
-			<S.Container
-				style={{
-					position: 'fixed',
-					inset: 0,
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					width: '100vw',
-					height: '100vh',
-					background: 'rgba(28, 28, 30, 0.5)',
-					zIndex: 9999,
-				}}
-			>
-				<S.ContentWrapper
-					style={{ height: 'auto', padding: '40px 32px', borderRadius: '16px', backgroundColor: '#ffffff', textAlign: 'center' }}
-				>
-					{props.children}
-				</S.ContentWrapper>
-			</S.Container>
+			<>
+				<S.ModalBackdrop />
+				<S.Container>
+					<S.ContentWrapper
+						style={{
+							height: 'auto',
+							padding: '40px 32px',
+							borderRadius: '16px',
+							backgroundColor: '#ffffff',
+							textAlign: 'center',
+						}}
+					>
+						{props.children}
+					</S.ContentWrapper>
+				</S.Container>
+			</>
 		</ModalPortal>
 	);
 };
