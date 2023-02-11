46a8efc3ceb3a1207d58a53dc54cbe4193f4dfc1
youngmin wi <youngminieo1005@gmail.com>
2023-02-11 15:12:56 +0900

    :sparkle: add get & delete about store API

---
+++ hooks/api/store/useGetStore.ts
@@ -0,0 +1,59 @@
+import { APIResponse } from 'types/api';
+import { API_PATH, HTTP_METHOD } from 'core/apis/constants';
+import pumpClientRequester from 'core/apis/axios';
+import { getUserTokenFromLocalStorage } from 'utils/storage';
+import { useQuery } from '@tanstack/react-query';
+
+type StoreImage = {
+	id: number;
+	storeId: number;
+	path: string;
+};
+
+export type Store = {
+	createdAt: string;
+	modifiedAt: string;
+	id: number;
+	userId: number;
+	name: string;
+	status: string;
+	latitude: string;
+	longitude: string;
+	businessHours: string;
+	notice: string;
+	address: string;
+	instaAccount: string;
+	callNumber: string;
+	registrationNumber: string;
+	isDayOff: boolean;
+	isReady: boolean;
+	imgStore: StoreImage[];
+};
+
+export interface StoreGetResponse extends APIResponse {
+	data: {
+		result: Store | null;
+	};
+}
+
+export const getStore = async () => {
+	const {
+		user: { store },
+	} = API_PATH;
+
+	const response = await pumpClientRequester<StoreGetResponse>({
+		method: HTTP_METHOD.GET,
+		url: `${store}`,
+		headers: {
+			Authorization: `Bearer ${getUserTokenFromLocalStorage()}`,
+		},
+	});
+
+	return response.data.data.result;
+};
+
+export const getStoreQueryKey = 'get/store';
+
+export const useGetStore = (changeTrigger: boolean) => {
+	return useQuery([getStoreQueryKey, changeTrigger], async () => await getStore());
+};
