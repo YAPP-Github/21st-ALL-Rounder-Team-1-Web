f34b869d15455429b8740f5fb6ea7e0d35232253
youngmin wi <youngminieo1005@gmail.com>
2023-02-11 15:14:48 +0900

    :sparkle: apply get & delete about store API in mypage/store

--- components/feature/MyPageSection/MyPageSectionRegisteredStore/index.tsx
+++ components/feature/MyPageSection/MyPageSectionRegisteredStore/index.tsx
@@ -4,29 +4,39 @@ import * as S from './styled';
 import { theme } from 'styles';
 import { TrashIcon } from 'public/static/icons';
 import useModalStore, { MODAL_KEY } from 'store/actions/modalStore';
+import { Store } from 'hooks/api/store/useGetStore';
+import { convertToHyphenDateFromUTC } from 'utils/date';
+import { MyPageStoreDefualtImg } from 'public/static/images';
 
-const MyPageSectionRegisteredStore = () => {
+const MyPageSectionRegisteredStore = ({ store }: { store: Store }) => {
+	const { name, address, callNumber, isReady, imgStore, modifiedAt } = store;
+
 	const { changeModalKey } = useModalStore();
 
 	return (
 		<S.RegisteredStoreContainer>
 			<S.RegisteredStoreContentWrapper>
 				<StyledLayout.FlexBox>
-					<S.RegisteredStoreImage src={''} alt={`${'서버데이터 - 상점 이름'}상점`} width={100} height={140} />
+					<S.RegisteredStoreImage
+						src={imgStore?.length ? imgStore[0].path : MyPageStoreDefualtImg}
+						alt={`${'서버데이터 - 상점 이름'}상점`}
+						width={100}
+						height={140}
+					/>
 				</StyledLayout.FlexBox>
 
 				<StyledLayout.FlexBox flexDirection={'column'} flex={'1'} margin="0 16px">
 					<Typography variant="span" aggressive="headline_oneline_004" margin="0 0 16px 0">
-						{true && '리필스테이션 매장이름'}
+						{name ?? '리필스테이션 매장이름'}
 					</Typography>
 
-					{false ? (
+					{isReady ? (
 						<>
 							<Typography variant="span" aggressive="body_oneline_004" margin="0 0 8px 0" color={theme.colors.gray_005}>
-								용인시 기흥구 신갈동 38-2
+								{address}
 							</Typography>
 							<Typography variant="span" aggressive="body_oneline_004" margin="0 0 8px 0" color={theme.colors.gray_005}>
-								02.312.4839
+								{callNumber}
 							</Typography>
 						</>
 					) : (
@@ -37,7 +47,7 @@ const MyPageSectionRegisteredStore = () => {
 				</StyledLayout.FlexBox>
 
 				<StyledLayout.FlexBox flexDirection={'column'} justifyContent={'flex-start'} gap={'8px'}>
-					{false ? (
+					{isReady ? (
 						<>
 							<MyPageActionBtn buttonTheme="black" type="button" onClick={() => console.info('판매제품 수정 GO')}>
 								판매제품 수정
@@ -63,16 +73,18 @@ const MyPageSectionRegisteredStore = () => {
 				</StyledLayout.FlexBox>
 			</S.RegisteredStoreContentWrapper>
 
-			<StyledLayout.FlexBox alignItems={'center'} justifyContent={'flex-end'} padding={'16px 28px'}>
-				<StyledLayout.FlexBox gap="20px">
-					<Typography variant="span" aggressive="body_oneline_005" color={theme.colors.gray_005}>
-						마지막 수정일
-					</Typography>
-					<Typography variant="span" aggressive="body_oneline_005" color={theme.colors.gray_005}>
-						2022년 12월 22일
-					</Typography>
+			{modifiedAt && (
+				<StyledLayout.FlexBox alignItems={'center'} justifyContent={'flex-end'} padding={'16px 28px'}>
+					<StyledLayout.FlexBox gap="20px">
+						<Typography variant="span" aggressive="body_oneline_005" color={theme.colors.gray_005}>
+							마지막 수정일
+						</Typography>
+						<Typography variant="span" aggressive="body_oneline_005" color={theme.colors.gray_005}>
+							{convertToHyphenDateFromUTC(modifiedAt)}
+						</Typography>
+					</StyledLayout.FlexBox>
 				</StyledLayout.FlexBox>
-			</StyledLayout.FlexBox>
+			)}
 		</S.RegisteredStoreContainer>
 	);
 };
