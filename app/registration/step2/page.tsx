162a22c92a6560057d29172214601ec880d440e5
ChoiSeYoung <sychoi0516@naver.com>
2023-02-12 15:49:14 +0900

    :ambulance: 충돌 해결

--- app/registration/step2/page.tsx
+++ app/registration/step2/page.tsx
@@ -257,9 +257,9 @@ const Step2 = () => {
 							<Typography variant="p" aggressive="body_oneline_004" color={theme.colors.gray_005}>
 								가게 사진이 없다면 기본 이미지로 등록해드려요
 							</Typography>
-	        {selectedStoreImageBtn === 'defaultImage' && (
-							<Image src={StoreDefaultImg} alt="기본가게이미지" width={343} height={160} style={{ paddingTop: '8px' }} />
-						)}
+							{selectedStoreImageBtn === 'defaultImage' && (
+								<Image src={StoreDefaultImg} alt="기본가게이미지" width={343} height={160} style={{ paddingTop: '8px' }} />
+							)}
 						</StyledLayout.FlexBox>
 					</StyledLayout.FlexBox>
 					<StyledLayout.FlexBox>
@@ -292,114 +292,114 @@ const Step2 = () => {
 							)}
 						</StyledLayout.FlexBox>
 					</StyledLayout.FlexBox>
-</StyledLayout.TextFieldSection>
-			<StyledLayout.TextFieldSection>
-				<label htmlFor="PromotionalChannel">
-					<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
-						홍보 채널 (선택)
-					</Typography>
-				</label>
-				<TextField name="step2" id="PromotionalChannel" inputFlag="normal" width="320px" />
-				<Typography variant="p" aggressive="body_oneline_004" color={theme.colors.gray_005}>
-					인스타그램, 블로그, 홈페이지 중 가장 활발히 사용하고 있는 채널 하나를 선택해서 링크 입력해주세요
-				</Typography>
-			</StyledLayout.TextFieldSection>
-			<StyledLayout.TextFieldSection>
-				<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
-					운영 시간
-				</Typography>
-				<StyledLayout.FlexBox gap="24px" style={{ padding: '4px 0' }}>
-					<StyledLayout.FlexBox gap="8px" alignItems="center">
-						<RadioBtn name="businessHour" value="weekDaysWeekEnd" onChange={handleSelectedBusinessHourBtn} defaultChecked />
-						<label htmlFor="weekDaysWeekEnd">
-							<Typography variant="h2" aggressive="button_001" color={theme.colors.gray_006}>
-								평일 / 주말 달라요
-							</Typography>
-						</label>
-					</StyledLayout.FlexBox>
-					<StyledLayout.FlexBox gap="8px" alignItems="center">
-						<RadioBtn name="businessHour" value="eachDays" onChange={handleSelectedBusinessHourBtn} />
-						<label htmlFor="eachDays">
-							<Typography variant="h2" aggressive="button_001" color={theme.colors.gray_006}>
-								요일별로 달라요
-							</Typography>
-						</label>
-					</StyledLayout.FlexBox>
-				</StyledLayout.FlexBox>
-				{selectedBusinessHourBtn === 'weekDaysWeekEnd' ? (
-					<StyledLayout.FlexBox flexDirection="column" gap="12px">
-						<StyledLayout.FlexBox>
-							<StyledLayout.FlexBox flexDirection="column" gap="6px">
-								<Typography variant="h3" aggressive="button_001" color="gray_007">
-									평일
-								</Typography>
-								<Typography variant="h4" aggressive="body_oneline_004" color="gray_005" margin="0 20px 0 0">
-									(월~금)
-								</Typography>
-							</StyledLayout.FlexBox>
-							<TimePicker dayOffRef={(el) => (dayOffRef.current[0] = el)} name="weekDays" />
-						</StyledLayout.FlexBox>
-						<StyledLayout.FlexBox>
-							<StyledLayout.FlexBox flexDirection="column" gap="6px">
-								<Typography variant="h3" aggressive="button_001" color="gray_007">
-									주말
-								</Typography>
-								<Typography variant="h4" aggressive="body_oneline_004" color="gray_005" margin="0 20px 0 0">
-									(토~일)
-								</Typography>
-							</StyledLayout.FlexBox>
-							<TimePicker dayOffRef={(el) => (dayOffRef.current[1] = el)} name="WeekEnd" disabled={dayOffStatus[0]} />
-							<DayOffBtn dayOff={dayOffStatus[0]} onClick={() => handleTimePickerInput(0)} style={{ marginLeft: '6px' }} />
-						</StyledLayout.FlexBox>
-					</StyledLayout.FlexBox>
-				) : (
-					<StyledLayout.FlexBox flexDirection="column" gap="12px">
-						{businessHourDays.map(({ id, day }) => {
-							return (
-								<StyledLayout.FlexBox key={id}>
-									<StyledLayout.FlexBox flexDirection="column" gap="6px">
-										<Typography variant="h3" aggressive="button_001" color="gray_007" margin="0 20px 0 0">
-											{day}
-										</Typography>
-									</StyledLayout.FlexBox>
-									<TimePicker dayOffRef={(el) => (dayOffRef.current[id] = el)} disabled={dayOffStatus[id - 1]} />
-									<DayOffBtn
-										dayOff={dayOffStatus[id - 1]}
-										onClick={() => handleTimePickerInput(id - 1)}
-										style={{ marginLeft: '6px' }}
-									/>
-								</StyledLayout.FlexBox>
-							);
-						})}
-					</StyledLayout.FlexBox>
-				)}
-			</StyledLayout.TextFieldSection>
-			<StyledLayout.TextFieldSection>
-				<label htmlFor="dayOff">
-					<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
-						휴무일
-					</Typography>
-				</label>
-				<TextField
-					emptyErrorMessage="휴무일을"
-					name="step2"
-					id="dayOff"
-					inputFlag={selectedStoreImageBtn === 'defaultImage' ? inputArr[7] : inputArr[8]}
-					onFocus={() => (selectedStoreImageBtn === 'defaultImage' ? changeNormal(7) : changeNormal(8))}
-					width="320px"
-				/>
-				<StyledLayout.FlexBox style={{ paddingTop: '4px' }}>
+				</StyledLayout.TextFieldSection>
+				<StyledLayout.TextFieldSection>
+					<label htmlFor="PromotionalChannel">
+						<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
+							홍보 채널 (선택)
+						</Typography>
+					</label>
+					<TextField name="step2" id="PromotionalChannel" inputFlag="normal" width="320px" />
 					<Typography variant="p" aggressive="body_oneline_004" color={theme.colors.gray_005}>
-						ex) 연중 무휴, 매주 토요일, 매달 둘째 및 넷째주 토요일 등
+						인스타그램, 블로그, 홈페이지 중 가장 활발히 사용하고 있는 채널 하나를 선택해서 링크 입력해주세요
 					</Typography>
+				</StyledLayout.TextFieldSection>
+				<StyledLayout.TextFieldSection>
+					<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
+						운영 시간
+					</Typography>
+					<StyledLayout.FlexBox gap="24px" style={{ padding: '4px 0' }}>
+						<StyledLayout.FlexBox gap="8px" alignItems="center">
+							<RadioBtn name="businessHour" value="weekDaysWeekEnd" onChange={handleSelectedBusinessHourBtn} defaultChecked />
+							<label htmlFor="weekDaysWeekEnd">
+								<Typography variant="h2" aggressive="button_001" color={theme.colors.gray_006}>
+									평일 / 주말 달라요
+								</Typography>
+							</label>
+						</StyledLayout.FlexBox>
+						<StyledLayout.FlexBox gap="8px" alignItems="center">
+							<RadioBtn name="businessHour" value="eachDays" onChange={handleSelectedBusinessHourBtn} />
+							<label htmlFor="eachDays">
+								<Typography variant="h2" aggressive="button_001" color={theme.colors.gray_006}>
+									요일별로 달라요
+								</Typography>
+							</label>
+						</StyledLayout.FlexBox>
+					</StyledLayout.FlexBox>
+					{selectedBusinessHourBtn === 'weekDaysWeekEnd' ? (
+						<StyledLayout.FlexBox flexDirection="column" gap="12px">
+							<StyledLayout.FlexBox>
+								<StyledLayout.FlexBox flexDirection="column" gap="6px">
+									<Typography variant="h3" aggressive="button_001" color="gray_007">
+										평일
+									</Typography>
+									<Typography variant="h4" aggressive="body_oneline_004" color="gray_005" margin="0 20px 0 0">
+										(월~금)
+									</Typography>
+								</StyledLayout.FlexBox>
+								<TimePicker dayOffRef={(el) => (dayOffRef.current[0] = el)} name="weekDays" />
+							</StyledLayout.FlexBox>
+							<StyledLayout.FlexBox>
+								<StyledLayout.FlexBox flexDirection="column" gap="6px">
+									<Typography variant="h3" aggressive="button_001" color="gray_007">
+										주말
+									</Typography>
+									<Typography variant="h4" aggressive="body_oneline_004" color="gray_005" margin="0 20px 0 0">
+										(토~일)
+									</Typography>
+								</StyledLayout.FlexBox>
+								<TimePicker dayOffRef={(el) => (dayOffRef.current[1] = el)} name="WeekEnd" disabled={dayOffStatus[0]} />
+								<DayOffBtn dayOff={dayOffStatus[0]} onClick={() => handleTimePickerInput(0)} style={{ marginLeft: '6px' }} />
+							</StyledLayout.FlexBox>
+						</StyledLayout.FlexBox>
+					) : (
+						<StyledLayout.FlexBox flexDirection="column" gap="12px">
+							{businessHourDays.map(({ id, day }) => {
+								return (
+									<StyledLayout.FlexBox key={id}>
+										<StyledLayout.FlexBox flexDirection="column" gap="6px">
+											<Typography variant="h3" aggressive="button_001" color="gray_007" margin="0 20px 0 0">
+												{day}
+											</Typography>
+										</StyledLayout.FlexBox>
+										<TimePicker dayOffRef={(el) => (dayOffRef.current[id] = el)} disabled={dayOffStatus[id - 1]} />
+										<DayOffBtn
+											dayOff={dayOffStatus[id - 1]}
+											onClick={() => handleTimePickerInput(id - 1)}
+											style={{ marginLeft: '6px' }}
+										/>
+									</StyledLayout.FlexBox>
+								);
+							})}
+						</StyledLayout.FlexBox>
+					)}
+				</StyledLayout.TextFieldSection>
+				<StyledLayout.TextFieldSection>
+					<label htmlFor="dayOff">
+						<Typography variant="h2" aggressive="body_oneline_004" color={theme.colors.gray_005}>
+							휴무일
+						</Typography>
+					</label>
+					<TextField
+						emptyErrorMessage="휴무일을"
+						name="step2"
+						id="dayOff"
+						inputFlag={selectedStoreImageBtn === 'defaultImage' ? inputArr[7] : inputArr[8]}
+						onFocus={() => (selectedStoreImageBtn === 'defaultImage' ? changeNormal(7) : changeNormal(8))}
+						width="320px"
+					/>
+					<StyledLayout.FlexBox style={{ paddingTop: '4px' }}>
+						<Typography variant="p" aggressive="body_oneline_004" color={theme.colors.gray_005}>
+							ex) 연중 무휴, 매주 토요일, 매달 둘째 및 넷째주 토요일 등
+						</Typography>
+					</StyledLayout.FlexBox>
+				</StyledLayout.TextFieldSection>
+				<StyledLayout.FlexBox justifyContent="center" style={{ paddingTop: '16px' }}>
+					<LargeBtn type="submit" style={style.btnStyle.primary_btn_002}>
+						다음단계
+					</LargeBtn>
 				</StyledLayout.FlexBox>
-			</StyledLayout.TextFieldSection>
-			<StyledLayout.FlexBox justifyContent="center" style={{ paddingTop: '16px' }}>
-				<LargeBtn type="submit" style={style.btnStyle.primary_btn_002}>
-					다음단계
-				</LargeBtn>
-			</StyledLayout.FlexBox>
-		</form>
+			</form>
 		</>
 	);
 };
