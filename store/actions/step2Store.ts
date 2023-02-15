71c9dfd7e618ec929b85fc9d84eade29e1ed4742
ChoiSeYoung <sychoi0516@naver.com>
2023-02-16 07:05:05 +0900

    :hammer: step2 zustand store 리팩토링

--- store/actions/step2Store.ts
+++ store/actions/step2Store.ts
@@ -1,53 +1,45 @@
+import { StoreData } from 'hooks/api/store/usePostStore';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-// TODO: step2 Store 리팩토링
-// export type Step2Request = {
-// 	name: string;
-// 	latitude: number;
-// 	longitude: number;
-// 	businessHour: string;
-// 	notice: string;
-// 	address: string;
-// 	imgPath: string[];
-// 	instaAccount: string;
-// 	callNumber: string;
-// 	registrationNumber: string;
-// };
-
-// export interface Step2Store {
-// 	Step2Request: Step2Request;
-// 	setStep2Request: (inputId: string, inputValue: string) => void;
-// }
-// export interface Step1Error {
-// 	[index: string]: Function | Object;
-// 	name: { value: string; isError: 'normal' | 'error' };
-// 	email: { value: string; isError: 'normal' | 'error' };
-// 	phoneNumber: { value: string; isError: 'normal' | 'error' };
-// 	changeError: (inputId: string) => void;
-// 	changeNormal: (inputId: string) => void;
-// 	setinitialValue: (inputId: string, inputValue: string) => void;
-// }
-// const initialState: Step2Request = {
-// 	name: '',
-// 	latitude: 0,
-// 	longitude: 0,
-// 	businessHour: '',
-// 	notice: '',
-// 	address: '',
-// 	imgPath: [''],
-// 	instaAccount: '',
-// 	callNumber: '',
-// 	registrationNumber: '',
-// };
-export interface stepInputs {
-	inputArr: Array<'normal' | 'error'>;
-	changeError: (id: number) => void;
-	changeNormal: (id: number) => void;
+export interface Step2Store {
+	step2Request: StoreData;
+	setStep2Request: (inputId: string, inputValue: string) => void;
 }
 
-export const useStep2Store = create<stepInputs>((set, get) => ({
-	inputArr: ['normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal'],
-	changeError: (id) => set((state) => ({ inputArr: { ...state.inputArr, [id]: 'error' } })),
-	changeNormal: (id) => get().inputArr[id] === 'error' && set((state) => ({ inputArr: { ...state.inputArr, [id]: 'normal' } })),
-}));
+const initialState: StoreData = {
+	name: '',
+	latitude: '',
+	longitude: '',
+	businessHours: '',
+	notice: '',
+	address: '',
+	imgPath: [''],
+	instaAccount: '',
+	callNumber: '',
+	registrationNumber: '',
+};
+
+export interface Step2Error {
+	[index: string]: Function | Object;
+	name: { value: string; isError: 'normal' | 'error' };
+	latitude: string;
+	longitude: string;
+	businessHours: string;
+	notice: { value: string; isError: 'normal' | 'error' };
+	address: { value: string; isError: 'normal' | 'error' };
+	imgPath: { value: string[]; isError: 'normal' | 'error' };
+	instaAccount: { value: string };
+	callNumber: { value: string; isError: 'normal' | 'error' };
+	registrationNumber: { value: string; isError: 'normal' | 'error' };
+	changeError: (inputId: string) => void;
+	changeNormal: (inputId: string) => void;
+	setInitialValue: (data: StoreData) => void;
+}
+export const step1RequestStore = create<Step2Store>()(
+	devtools((set) => ({
+		step2Request: initialState,
+		setStep2Request: (inputId: string, inputValue: string) =>
+			set((state) => ({ step2Request: { ...state.step2Request, [inputId]: inputValue } })),
+	})),
+);
