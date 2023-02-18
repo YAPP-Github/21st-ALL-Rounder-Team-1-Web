374c7c203653733adb631c62ead7882dfe1f57fe
위영민 <youngminieo1005@gmail.com>
2023-02-19 01:51:41 +0900

    :adhesive_bandage: small change app root head file

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,9 @@
 export default function Head() {
 	return (
 		<>
-			<title>Pump</title>
+			<title>Pump ㅣ 내 주변의 리필스테이션</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta name="description" content="자연스럽게 리필 용기를 챙기는 날을 만드는 사람들" />
 			<link rel="shortcut icon" href="/favicon.ico" />
 		</>
 	);
