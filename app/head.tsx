6052ac8bf532880a0901c4fd552bb350ffcc6d51
youngmin wi <youngminieo1005@gmail.com>
2023-02-21 22:41:53 +0900

    chore: small change

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
 		<>
 			<title>Pump ㅣ 내 주변의 리필스테이션</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 			<meta name="description" content="자연스럽게 리필 용기를 챙기는 날을 만드는 사람들" />
 			<link rel="shortcut icon" href="/favicon.ico" />
 		</>
